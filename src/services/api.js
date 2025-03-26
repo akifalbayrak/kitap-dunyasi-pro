0a82bcbb893c3eb5f3413d3c92795e15e17a5d6c
akif albayrak <akifalbayrak6161@gmail.com>
2025-03-26 14:57:29 +0300

    feat(api): refactor API base URL to use params for API key

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const API_KEY = "fca_live_B0ZftLGW9xq4r0CmkK0KCZu78T54zNANyEUaqA7u";
 // API base URL
 const api = axios.create({
-    baseURL: `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_B0ZftLGW9xq4r0CmkK0KCZu78T54zNANyEUaqA7u`,
+    baseURL: `https://api.freecurrencyapi.com/v1/latest`,
+    params: {
+        apikey: API_KEY,
+    },
 });
 
 // Error handling interceptor
